02d75d81d1d7faf1566d7af0ab7a486b9944ba18
fengchuiguo1994 <huang182@live.cn>
2019-09-04 15:56:54 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,17 +7,22 @@ bedGraphToBigWig output/out.frag.bedgraph ~/Genome/10Xgenome/dm3/dm3.chrom.sizes
 computeMatrix scale-regions -S output/out.frag.bw -R ~/Genome/10Xgenome/dm3/dm3.bed -a 2000 -b 2000 -m 2000 --skipZeros --outFileName output/out.frag.profile.gz --numberOfProcessors 4
 plotHeatmap -m output/out.frag.profile.gz -out output/out.frag.profile.pdf --colorMap GnBu
 
+# tian：不仅仅考虑了barcode ID，也考虑了测序的机器，*bed.q30.lenBE50.CHR_CLEAN.5e0_3e500.EXT.BCline.GAPEXT3000.FRAGMENT_GEMLIN后续的文件都能反应
+# eg：H:fragnumH1|human-10000-100000002-AAACACCAGAAACCTABX1-E00515;chr11      75891303        75891934        2       human-10000-100000002-AAACACCAGAAACCTABX1-E00515-FRAG0000
+# eg：H-A:fragnumH2-chrnum1|human-10000-100000107-AAACACCAGCTTACACBX1-E00515;chr2     32916229        32916792        1       human-10000-100000107-AAACACCAGCTTACACBX1-E00515-FRAG0000;chr2  235142786       235145352       13      human-10000-100000107-AAACACCAGCTTACACBX1-E00515-FRAG0001
+# 我的只考虑了barcode ID
 
-## 田：(只保留了primary hit) 我的保留了secondary hit (STEP0401_F2304.run.p)
+## tian：(只保留了primary hit) 我的保留了secondary hit (STEP0401_F2304.run.p)
 # samtools view -h -F 2304 $F|samtools view -hbS - > $file.F2304.bam &
 
-## 田：(没有取唯一比对的结果) 我的增加了提取唯一比对的结果 (AS!=XS) (PX_0403_SH_BCLINE.sh)
-## 田：(去掉了部分染色体)  我的全部保留下来了
+## tian：(没有取唯一比对的结果) 我的增加了提取唯一比对的结果 (AS!=XS) (PX_0403_SH_BCLINE.sh)
+## tian：(去掉了部分染色体)  我的全部保留下来了
 # cat $F|awk '{if($5>=30) print}' > $file.q30
 # cat $file.q30|awk '{if($3-$2>=50)print}' > $file.q30.lenBE50
 # cat $file.q30.lenBE50 |grep -v "EBV"|grep -v "_random"|grep -v "chrUn"|grep -v "hs"|grep -v "Het"|grep -v "chrU"|grep -v "chrM"|grep -v "extra" > $file.q30.lenBE50.CHR_CLEAN
+# 现在的也增加了black chromsome，可以去掉
 
-## 田：(延伸500bp是 +:5-3 -:3-5) 我的是都是从5-3延伸500bp (PX_0403_EXT_R1_3e500.py)
+## tian：(延伸500bp是 +:5-3 -:3-5) 我的是都是从5-3延伸500bp (PX_0403_EXT_R1_3e500.py)
 # word=line.rstrip("\n").split("\t")
 # S=word[1]
 # E=word[2]
@@ -25,4 +30,4 @@ plotHeatmap -m output/out.frag.profile.gz -out output/out.frag.profile.pdf --col
 # EXTS=int(S)-int(EXT5)
 # EXTE=int(E)+int(EXT3)
 
-## 田：(将跨染色体的结果分成了自连接和染色体间链接) 我的是全部考虑 (STEP040503_CLASS_GEM2EAS.run.p/STEP040504_CLASS_E2AS.run.p/STEP040505_COMBINE_SA.run)
+## tian：(将跨染色体的结果分成了自连接和染色体间链接) 我的是全部考虑 (STEP040503_CLASS_GEM2EAS.run.p/STEP040504_CLASS_E2AS.run.p/STEP040505_COMBINE_SA.run)
